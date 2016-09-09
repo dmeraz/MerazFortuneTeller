120d3e4ae91844eea58e549498fe8a8f6d209dc8
dmeraz <darin.meraz@gmail.com>
2016-09-09 16:09:38 -0400

    added ReadLines for user input

--- Program.cs
+++ Program.cs
@@ -14,27 +14,41 @@ namespace MerazFortuneTeller
 
             Console.WriteLine("Please type your first name and hit enter.");
 
+            string firstName = Console.ReadLine();
+
             //Ask the user for the user’s last name
 
             Console.WriteLine("Please type your last name and hit enter.");
+
+            string lastName = Console.ReadLine();
 
             //Ask the user for the user’s age 
 
             Console.WriteLine("Please type your age and hit enter.");
 
+            int age = int.Parse(Console.ReadLine());
+
             //Ask the user for the user’s birth month(as an 'int')
 
             Console.WriteLine("Please type your birth month as a number from 1-12.");
 
+            int birthMonth = int.Parse(Console.ReadLine());
+
             //Ask the user for the user’s favorite ROYGBIV color
 
             Console.WriteLine("Please type your favorite ROYGBIV color. If you do not know what ROYGBIV is, type \"Help\" and hit enter.");
+
+            string favColor = Console.ReadLine();
+
+            //Set conditional to branch for color vs "Help"
 
             //If the user does not know what ROYGBIV is, ask them to enter “Help” to get a list of the ROYGBIV colors
 
             //Ask the user for the number of siblings the user has
 
             Console.WriteLine("Please type the number of siblings you have and hit enter.");
+
+            int siblings = int.Parse(Console.ReadLine());
         }
     }
 }
