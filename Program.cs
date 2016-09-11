44270c6b176417d7f9d79860e65416a75716491e
dmeraz <darin.meraz@gmail.com>
2016-09-10 20:06:38 -0400

    ROYGBIV switch case WORKS, no help

--- Program.cs
+++ Program.cs
@@ -31,13 +31,12 @@ namespace MerazFortuneTeller
 
             if (age % 2 == 0)
             {
-                retirementAge = "12";
+                retirementAge = "12 years";
             }
             else
             {
-                retirementAge = "17";
+                retirementAge = "17 years";
             }
-            
 
             //Ask the user for the user’s birth month(as an 'int')
 
@@ -45,6 +44,8 @@ namespace MerazFortuneTeller
 
             int birthMonth = int.Parse(Console.ReadLine());
             string moneyInBank;
+
+            //Conditionals ordered based on most common birth months in US
 
             if (birthMonth >= 5 && birthMonth <= 8)
             {
@@ -66,17 +67,51 @@ namespace MerazFortuneTeller
             {
                 moneyInBank = "$0.00";
             }
-                
+
 
             //Ask the user for the user’s favorite ROYGBIV color
 
-        Console.WriteLine("Please type your favorite ROYGBIV color. If you do not know what ROYGBIV is, type \"Help\" and hit enter.");
+            Console.WriteLine("Please type your favorite ROYGBIV color and hit enter. If you do not know what ROYGBIV is, type \"Help\" and hit enter.");
 
             string favColor = Console.ReadLine();
+            string transportMode;
 
             //Set conditional to branch for color vs "Help"
 
+            if ( favColor = "Help")
+
+                Console.WriteLine("According to Wikipedia, ROYGBIV or Roy G. Biv is an acronym"+
+                    " for the sequence of hues commonly described as making up a rainbow:"+
+                    " red, orange, yellow, green, blue, indigo and violet."+
+                    "/n /n Now, type your favorite ROYGBIV color and hit enter.");
+
             //If the user does not know what ROYGBIV is, ask them to enter “Help” to get a list of the ROYGBIV colors
+
+            switch (favColor)
+            {
+                case "red":
+                    transportMode = "space ship";
+                    break;
+                case "orange":
+                    transportMode = "train";
+                    break;
+                case "yellow":
+                    transportMode = "tractor";
+                    break;
+                case "green":
+                    transportMode = "teleportation machine";
+                    break;
+                case "blue":
+                    transportMode = "DUKW";
+                    break;
+                case "indigo":
+                    transportMode = "sports car";
+                    break;
+                case "violet":
+                    transportMode = "pirate ship";
+                    break;
+
+            }
 
             //Ask the user for the number of siblings the user has
 
@@ -89,8 +124,8 @@ namespace MerazFortuneTeller
             //a vacation home in [Location] and a [Mode of Transportation].
 
             Console.WriteLine(string.Concat(firstName + " " + lastName + " " + "will retire in " + retirementAge
-                + " with " + moneyInBank + " in the bank, a vacation home in " + "Location" + ", " +
-                "and a " + "mode of transportation" + ".")); //replace with actual variables!
+                + " with " + moneyInBank + " in the bank, a vacation home in " + "Location" + ", "
+                + "and a " + transportMode + ".")); //replace with actual variables!
 
         }
     }
